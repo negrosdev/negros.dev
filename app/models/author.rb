c644fceb2101eb155c0fdd5266984077e6fa696c
Marcus Pereira <mpereirassa@gmail.com>
2021-03-22 00:21:19 -0300

    link gravatar to author e-mail

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  def avatar
+    gravatar_id = Digest::MD5::hexdigest(eamil.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: name, class: 'is-rounded')
+  end
 end
