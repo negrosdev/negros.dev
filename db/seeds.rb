9ff4f456d0ba0f2b86d831f22b5e24b039fcc57c
Marcus Pereira <mpereirassa@gmail.com>
2021-03-24 00:38:48 -0300

    :update seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 author = Author.create(name: 'Marcus Pereira', email: 'student@negros.dev')
-category = Category.create(name: 'golang', slug: 'golang')
+category = Category.create(name: 'Ruby', slug: 'ruby')
 source_code = SourceCode.create(name: '283283-f', url: 'https://s.negros.dev/283283-f.zip')
-Course.create(title: 'Design Pattern - Factory Com Golang', 
-              slug: 'design-pattern-factory-com-golang',
+Course.create(title: 'Design Pattern - Factory Com Ruby', 
+              slug: 'design-pattern-factory-com-ruby',
               description: 'factory usango a linguagem go',
               level: 4,
               author: author,
