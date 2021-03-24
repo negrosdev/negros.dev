1a3dc6db2a3ce7fb1d65534d80e2095d3c9158e1
Marcus Pereira <mpereirassa@gmail.com>
2021-03-23 21:47:01 -0300

    :add relateds courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,5 +4,6 @@ class CoursesController < ApplicationController
 
   def show
     @course = Course.find(params[:id])
+    @relateds_courses = Course.where(category: @course.category).last(4)
   end
 end
--- spec/features/visitor/visitor_view_course_page_spec.rb
+++ spec/features/visitor/visitor_view_course_page_spec.rb
@@ -9,4 +9,15 @@ feature 'Visitor view course page' do
     expect(page).to have_content(course.title)
     expect(page).to have_content(course.author.name)
   end
+  scenario 'and view relateds courses' do
+    category = create(:category, name: 'ruby')
+    courses = create_list(:course, 4, category: category)
+
+    visit root_path
+    click_on courses.first.title
+
+    expect(page).to have_css('.splide__slide', count: 4)
+    expect(page).to have_content(courses.first.title)
+    expect(page).to have_content(courses.last.title)
+  end
 end
