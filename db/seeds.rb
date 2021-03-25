71a4c113a49d821e8a1661b2b13e6c1039eb978e
Marcus Pereira <mpereirassa@gmail.com>
2021-03-25 18:08:01 -0300

    :create more instancies of course object

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 author = Author.create(name: 'Marcus Pereira', email: 'student@negros.dev')
+author2 = Author.create(name: 'Aline Rosa', email: 'aline@yahoo.com')
+
 category = Category.create(name: 'Ruby', slug: 'ruby')
+category2 = Category.create(name: 'Ionic', slug: 'ionic')
+category3 = Category.create(name: 'React', slug: 'react')
+
 source_code = SourceCode.create(name: '283283-f', url: 'https://s.negros.dev/283283-f.zip')
+
+
 Course.create(title: 'Design Pattern - Factory Com Ruby', 
               slug: 'design-pattern-factory-com-ruby',
               description: 'factory usango a linguagem ruby',
@@ -10,3 +17,94 @@ Course.create(title: 'Design Pattern - Factory Com Ruby',
               category: category,
               code: 'c01uc81ja7'
             )
+
+Course.create(title: 'Criando Componentes Com React', 
+              slug: 'criando-componentes-com-reactjs',
+              description: 'criando componentes com reactjs',
+              level: 2,
+              author: author2,
+              source_code: source_code,
+              category: category3,
+              code: 'c01u31c21'
+            )
+
+Course.create(title: 'Ionic Desenvolvendo Mobile Apps ', 
+              slug: 'ionic-desenvolvendo-mobile-apps',
+              description: 'ionic',
+              level: 4,
+              author: author,
+              source_code: source_code,
+              category: category2,
+              code: '9812nmc'
+            )
+
+Course.create(title: 'React.js - State', 
+              slug: 'react-js-state',
+              description: 'reactjs state',
+              level: 5,
+              author: author2,
+              source_code: source_code,
+              category: category2,
+              code: '12o09adc'
+            )
+
+Course.create(title: 'Ionic Icons', 
+              slug: 'ionic-icons',
+              description: 'ionic icons',
+              level: 3,
+              author: author2,
+              source_code: source_code,
+              category: category2,
+              code: '1298casd9'
+            )
+
+Course.create(title: 'Entendendo O Rails New', 
+              slug: 'entendendo-o-rails-new',
+              description: 'rails new',
+              level: 1,
+              author: author,
+              source_code: source_code,
+              category: category,
+              code: '823908cn'
+            )
+
+Course.create(title: 'Ruby Agendamento', 
+              slug: 'ruby-agendamento-de-tarefas',
+              description: 'Trabalhando com agendamento de tarefas',
+              level: 4,
+              author: author,
+              source_code: source_code,
+              category: category,
+              code: 'sa-d901'
+            )
+
+Course.create(title: 'Instalando Ionic', 
+              slug: 'instalando-ionic',
+              description: 'aprenda a instalar o ionic',
+              level: 2,
+              author: author2,
+              source_code: source_code,
+              category: category2,
+              code: 'c01uc81ja7'
+            )
+
+Course.create(title: 'Configurando Ionic', 
+              slug: 'configurando-ionic',
+              description: 'configurando ionic',
+              level: 4,
+              author: author2,
+              source_code: source_code,
+              category: category2,
+              code: 'asdi912joasc'
+            )
+
+Course.create(title: 'Instalando React Create App', 
+              slug: 'instalando-react-create-app',
+              description: 'instalando react create app',
+              level: 1,
+              author: author2,
+              source_code: source_code,
+              category: category3,
+              code: 'a9s8d9a8'
+            )
+
