e7168a4b670a0633d7aac8355348cd2ea970f45a
Marcus Pereira <mpereirassa@gmail.com>
2021-03-25 17:51:36 -0300

    fix: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,10 @@ category = Category.create(name: 'Ruby', slug: 'ruby')
 source_code = SourceCode.create(name: '283283-f', url: 'https://s.negros.dev/283283-f.zip')
 Course.create(title: 'Design Pattern - Factory Com Ruby', 
               slug: 'design-pattern-factory-com-ruby',
-              description: 'factory usango a linguagem go',
+              description: 'factory usango a linguagem ruby',
               level: 4,
               author: author,
               source_code: source_code,
-              category: category
+              category: category,
+              code: 'c01uc81ja7'
             )
