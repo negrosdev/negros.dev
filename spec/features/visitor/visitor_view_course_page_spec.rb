65d41d7850d1eebf7f20544944ccd819dd8be4db
Marcus Pereira <mpereirassa@gmail.com>
2021-03-26 08:48:14 -0300

    :create more tests

--- spec/features/visitor/visitor_view_course_page_spec.rb
+++ spec/features/visitor/visitor_view_course_page_spec.rb
@@ -9,7 +9,19 @@ feature 'Visitor view course page' do
     expect(page).to have_content(course.title)
     expect(page).to have_content(course.author.name)
   end
-  scenario 'and view relateds courses' do
+  scenario 'and view recommended courses' do
+    category = create(:category, name: 'ruby')
+    courses = create_list(:course, 4, category: category)
+
+    visit root_path
+    click_on courses.first.title
+
+    expect(page).to have_content('Recomendados')
+    expect(page).to have_content(courses.first.title)
+    expect(page).to have_content(courses.last.title)
+  end
+
+  scenario 'and count recommended courses' do
     category = create(:category, name: 'ruby')
     courses = create_list(:course, 4, category: category)
 
@@ -17,7 +29,5 @@ feature 'Visitor view course page' do
     click_on courses.first.title
 
     expect(page).to have_css('.splide__slide', count: 4)
-    expect(page).to have_content(courses.first.title)
-    expect(page).to have_content(courses.last.title)
   end
 end
