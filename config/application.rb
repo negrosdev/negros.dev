03f39c85e9fdd967f9927553c72c89f82bd6735c
Marcus Pereira <mpereirassa@gmail.com>
2021-04-10 22:42:14 -0300

    :set 404 and 500 custom pages

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Negrosdev
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
   mount RailsAdmin::Engine => '/supervisor', as: 'rails_admin'
 
   devise_for :users, only: :sessions
