9ed81e19625eafbba70a189c97a101d68a9fc614
Marcus Pereira <mpereirassa@gmail.com>
2021-04-12 21:59:22 -0300

    :create contribute page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def contribute
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 
+  get 'como-contribuir', to: 'pages#contribute'
+
   mount RailsAdmin::Engine => '/supervisor', as: 'rails_admin'
 
   devise_for :users, only: :sessions
