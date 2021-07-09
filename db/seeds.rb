575592c2e5d1283f7ac316a86dd1c330ad166af3
Marcus Pereira <mpereirassa@gmail.com>
2021-07-09 14:20:59 -0300

    seeds remounted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 User.create(email: 'user@negros.dev', password: '123456', role: 6)
 
-FactoryBot.create(:content,
-                  title: "Criando Apis com Go",
-                  tag: FactoryBot.create(:tag),
-                  author: FactoryBot.create(:author, :with_photo))
+tag = FactoryBot.create(:tag, name: 'Python')
+content = FactoryBot.create(:content,
+                            title: "Desenvolvendo APIs com Django Restfull",
+                            tag: FactoryBot.create(:tag),
+                            description: "Neste conteúdo iremos abordar a criação de apis restfull usando o framework Django.",
+                            tag: tag,
+                            author: FactoryBot.create(:author, :with_photo))
+tracks = FactoryBot.create_list(:track, 5, content: content)
+content.thumbnail.attach(io: File.open('spec/support/assets/django.png'), filename: 'django.png', content_type: 'image/png')
