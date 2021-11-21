e044c48d0701875ddbfb5e751d53c765ca490066
Marcus Pereira <marcus@negros.dev>
2021-11-21 10:58:42 -0300

    build: add flask-seeder extension

--- app/extensions/extensions.py
+++ app/extensions/extensions.py
@@ -1,9 +1,11 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_seeder import FlaskSeeder
 
 db = SQLAlchemy()
 migrate = Migrate()
+seeder = FlaskSeeder()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 
@@ -12,3 +14,4 @@ def init_app(app):
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+    seeder.init_app(app, db)
---
+++ seeds/user.py
@@ -0,0 +1,24 @@
+from flask_seeder import Seeder, Faker, generator
+
+from app.models.user import User, ERole
+
+class UserSeeder(Seeder):
+  def __init__(self, db=None):
+    super().__init__(db=db)
+    self.priority = 1
+
+  def run(self):
+    faker = Faker(
+      cls=User,
+      init={
+        "name": generator.Name(),
+        "email": "admin@negros.dev",
+        "password": "admin123",
+        "description": generator.Sequence(),
+        "role": ERole.ADMIN,
+      }
+    )
+
+    for user in faker.create():
+        print("Adding user: %s" % user)
+        self.db.session.add(user)
