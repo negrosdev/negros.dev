7ab3561eb089ff767f80bb80dc84dc46e89dc310
Marcus Almeida <mpereirassa@gmail.com>
2023-07-17 14:22:21 -0300

    chore(rails_helper.rb): add FactoryBot::Syntax::Methods to RSpec configuration to enable the use of FactoryBot methods in tests (#69)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
