47a07ebd7ad14b8ab327d41f1b85f968820b635c
Marcus Pereira <mpereirassa@gmail.com>
2023-07-17 14:10:01 -0300

    chore(rails_helper.rb): add configuration for Shoulda Matchers to integrate with RSpec and Rails test framework

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -19,4 +20,11 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
