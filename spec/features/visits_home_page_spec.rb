0bb61189e953980e31b643e4433df3ee2f904ca2
Marcus Pereira <mpereirassa@gmail.com>
2023-07-18 18:41:18 -0300

    feat(views): add navbar and footer partials to application layout for consistent UI across pages feat(views): create _navbar.html.erb partial to display site logo and name in the navbar feat(views): create _footer.html.erb partial to display copyright information in the footer test(views): add feature tests to ensure navbar and footer are displayed correctly on the home page test(views): add scenario to check if the page has a successful HTTP status test(views): add scenario to check if the page has the correct title test(views): add scenario to check if the page has the correct footer content

--- spec/features/visits_home_page_spec.rb
+++ spec/features/visits_home_page_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 feature 'Visits Home page' do
-  scenario 'should main texts' do
-    content = create(:content)
+  before {  visit root_path }
 
-    visit root_path
+  context 'when visit page successfully' do
+    scenario { expect(page).to have_http_status :success }
+  end
 
-    expect(page).to have_http_status :success
-    expect(page).to have_content 'Marcus Almeida'
-    expect(page).to have_content '&COPY; Marcus Almeida, 2023 · Todos os direitos reservados.'
+  context 'when visitors view title' do
+    scenario { expect(page).to have_content 'Negros.dev' }
+  end
+
+  context 'when visitors view footer' do
+    scenario { have_content '&COPY; Negros.dev, 2023 · Todos os direitos reservados.' }
   end
 end
