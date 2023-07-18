c68ff36c20261105db6aea87ab501d03e3822633
Marcus Pereira <mpereirassa@gmail.com>
2023-07-18 17:56:53 -0300

    fix(content.rb): add enum for status with values draft, publish, and review to enforce specific status values fix(contents.rb): change status value from integer to symbol :publish to match the enum definition in the model test(content_spec.rb): add tests to validate that the status enum is defined and has the correct values

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,4 +3,6 @@ class Content < ApplicationRecord
   friendly_id :name, use: :slugged
 
   validates :name, :body, :description, :slug, presence: true
+
+  enum status: {draft: 0, publish: 1, review: 2}
 end
