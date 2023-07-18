bc32364c40103c145d01f18271d1ef3464935441
Marcus Pereira <mpereirassa@gmail.com>
2023-07-18 17:38:52 -0300

    feat(content.rb): add FriendlyId gem and configure it to use the 'name' attribute as the slug for Content model
    feat(create_friendly_id_slugs.rb): create migration to add 'friendly_id_slugs' table to the database with necessary columns and indexes for FriendlyId gem
    
    chore(schema.rb): update schema version to reflect the new migration (2023_07_18_202617)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_173131) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_202617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_173131) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_contents_on_name"
     t.index ["slug"], name: "index_contents_on_slug", unique: true
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
 end
