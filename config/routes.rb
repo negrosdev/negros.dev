cdb25537b1d344263168b670bc4b579092bd5122
Marcus Almeida <mpereirassa@gmail.com>
2023-07-27 14:16:33 -0300

    feat(Gemfile/administrate): Criando página administrativa (#92)
    * feat(Gemfile/administrate): Criando página administrativa
    
    * fix(pico.css): Adicionando CSS não minificado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :contents
+
+      root to: "contents#index"
+    end
   root "home#index"
 
   resources :contents, only: %i[show]
