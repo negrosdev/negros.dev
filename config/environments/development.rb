1e6b9bc4f34655ca5ec316b7cf84808751550ee7
Marcus Pereira <mpereirassa@gmail.com>
2023-07-27 14:24:28 -0300

    feat(devise.rb): Configuração do Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
