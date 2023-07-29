332ef489ee61faf04cc8730b3a4043c97d60240f
Marcus Pereira <mpereirassa@gmail.com>
2023-07-29 15:22:45 -0300

    fix(user.rb): remove o módulo :recoverable do devise feat(sessions/new.html.erb): adiciona a página de login feat(application.html.erb): adiciona ícones de favicon e apple-touch-icon fix(devise.pt-BR.yml): corrige formatação do arquivo YAML

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
 
   enum roles: { student: 0, staff: 2, writer: 4, reviewer: 6, admin: 8 }
 
