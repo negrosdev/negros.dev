2f3a3664ae077b605b1339e4277bf68a97975f27
Marcus Pereira <mpereirassa@gmail.com>
2023-07-29 11:01:15 -0300

    chore(routes.rb): adiciona uma linha em branco entre as rotas do devise e o namespace admin chore(routes.rb): adiciona uma linha em branco entre o namespace admin e a rota root chore(routes.rb): adiciona uma linha em branco entre a rota root e as resources contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   namespace :admin do
       resources :contents
       resources :categories
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
 
       root to: "contents#index"
     end
+
   root "home#index"
 
   resources :contents, only: %i[show]
